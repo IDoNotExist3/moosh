cde3ecc61d41333f84b43c15ce612ee01f8f5038
IDoNotExist3 <exi@exyst.dev>
2025-04-24 17:21:59 -0400

    added callback id and place tracking and starting bulk

--- moosh.py
+++ moosh.py
@@ -3,6 +3,8 @@ import tkinter as tk
 import atexit
 from datetime import datetime
 
+editingReady = False
+
 conn = psycopg2.connect(database="moosh",
                         host="localhost",
                         user="postgres",
@@ -10,101 +12,175 @@ conn = psycopg2.connect(database="moosh",
                         port="5432")
 
 ####Functions
+
+def get_all_text_of_tasks(parent_widget):
+    print("printing children")
+    children_widgets = parent_widget.winfo_children()
+    for child_widget in children_widgets:
+        if child_widget.winfo_class() == 'Entry':
+            print("This is an entry")
+            children_widgets2 = child_widget.winfo_children()
+            for child_widget2 in children_widgets2:
+                if child_widget.winfo_class() == 'Entry':
+                    print(child_widget.get())
+        elif child_widget.winfo_class() == 'Label':
+            print()
+        elif child_widget.winfo_class() == 'Frame':
+            children_widgets2 = child_widget.winfo_children()
+       #      print(children_widgets2)
+       #      for child_widget2 in children_widgets2:
+       #          if child_widget2.winfo_class() == 'Entry':
+       #              print(child_widget2.get())
+
+def save_all_text_of_tasks(parent_widget):
+#     print("printing children")
+    children_widgets = parent_widget.winfo_children()
+    # print(len(children_widgets))
+    for child_widget in children_widgets:
+        if child_widget.winfo_class() == 'Entry':
+            print("This is an entry")
+            children_widgets2 = child_widget.winfo_children()
+            for child_widget2 in children_widgets2:
+                if child_widget.winfo_class() == 'Entry':
+                    print(child_widget.get())
+        elif child_widget.winfo_class() == 'Label':
+            # print(child_widget.text)
+            print()
+        elif child_widget.winfo_class() == 'Frame':
+            # print("This is frame")
+            children_widgets2 = child_widget.winfo_children()
+       #      print(children_widgets2)
+            for child_widget2 in children_widgets2:
+                if child_widget2.winfo_class() == 'Entry':
+                    print(child_widget2.get())
+                # elif child_widget2.winfo_class() == 'Label':
+                #     # print("Another label...")  
+
+def save():
+    save_all_text_of_tasks(tables)
+
 def taskCallback():
       print("1")
+#       get_all_text_of_tasks(tables)
+      return True
+jarCols = ["id","variety ","date_innoculated","shake_date","bulk_date","bulk_id","grain"]
+def jarCallback(id,place,value):
+      if editingReady:
+       #       print(id)
+       # #       get_all_text_of_tasks(tables)
+       #       print(place)
+       print(f'val: {value}, id: {id}, place: {place}')
+       #       sql = f"UPDATE jars SET {jarCols[place]} = %P"
+       tempCursor = conn.cursor()
+      else:
+          return True
+      return True
 
-def main():
-       ### Initialize Tkinter
-       root = tk.Tk()
-       sv = tk.StringVar()
+# def main():
+### Initialize Tkinter
+root = tk.Tk()
+sv = tk.StringVar()
 
-       frm = tk.Frame(master=root)
-
-
-       ######TESTING
-       def hello():   #Hello! menu
-              print("hello!")
-       def Hello():   #Quit! menu
-              print("Hello!")
-       # create a toplevel menu
-       menubar = tk.Menu(root)
-       menubar.add_command(label="Hello!", command=hello)
-       menubar.add_command(label="Quit!", command=Hello)
-       root.config(menu=menubar)
-
-       #####END TESTING
-
-       ###Frames
-       tables = tk.Frame(master=root,relief=tk.RIDGE,borderwidth=5)
-
-       ### Initalize postgreSQL cursor
-       cursor = conn.cursor()
+frm = tk.Frame(master=root)
 
 
-       ### Greeting
-       greetingTime = ["Night", "Morning", "Afternoon", "Evening"]
+######TESTING
+menubar = tk.Menu(root)
+menubar.add_command(label="Save", command=save, font=("Arial", 20))
+# menubar.add_command(label="Quit!", command=Hello)
+root.config(menu=menubar)
 
-       now = datetime.now().time()
+#####END TESTING
 
-       timing = int(now.hour/6)
+###Frames
+tables = tk.Frame(master=root,relief=tk.RIDGE,borderwidth=5)
 
-       greeting = tk.Label(
-              text=f"Good {greetingTime[timing]} Exi!",
-              font=("Arial", 50),
-              fg="steelblue",
-       )
+### Initalize postgreSQL cursor
+cursor = conn.cursor()
 
-       ### Fill tables
-       sql = "SELECT * from jars"
-       cursor.execute(sql)
-       colnames = [desc[0] for desc in cursor.description]
-       res = cursor.fetchall()
-       jar_titles = tk.Frame(master=tables)
-       for col in colnames:
-              text = tk.StringVar()
-              text.set(col)
-              tk.Label(
-                    text = col,
-                    master=jar_titles,
-                    width=20,
-              ).pack(side=tk.LEFT)
-              # tk.Entry(master=jar_titles,textvariable=text,validate="focusout",width=20).pack(side=tk.LEFT)
-       jar_titles.pack()
-       for line in res:
-              frm_tmp = tk.Frame(master=tables)
-              cnt = 0
-              for txt in line:
-                     text = tk.StringVar()
-                     text.set(txt)
-                     e =tk.Entry(master=frm_tmp,textvariable=text,validate="key",validatecommand=taskCallback,width=20)
-                     e._id = len(line)-1
-                     e.pack(side=tk.LEFT)
-                     cnt = cnt + 1
-              frm_tmp.pack()
 
-       ###Packing
-       greeting.pack()
-       tables.pack()
-       frm.pack()
-       
-       ### Begin program
-       print("Breakpoint")
-       root.title('Moosh')
-       root.geometry("1000x800")
-       root.mainloop()
+### Greeting
+greetingTime = ["Night", "Morning", "Afternoon", "Evening"]
+
+now = datetime.now().time()
+
+timing = int(now.hour/6)
+
+greeting = tk.Label(
+       text=f"Good {greetingTime[timing]} Exi!",
+       font=("Arial", 50),
+       fg="steelblue",
+)
+
+### Fill tables
+sql = "SELECT * from jars"
+cursor.execute(sql)
+colnames = [desc[0] for desc in cursor.description]
+res = cursor.fetchall()
+jar_titles = tk.Frame(master=tables)
+for col in colnames:
+       text = tk.StringVar()
+       text.set(col)
+       tk.Label(
+              text = col,
+              master=jar_titles,
+              width=20,
+       ).pack(side=tk.LEFT)
+       # tk.Entry(master=jar_titles,textvariable=text,validate="focusout",width=20).pack(side=tk.LEFT)
+jar_titles.pack()
+for line in res:
+       frm_tmp = tk.Frame(master=tables)
+       cnt = 0
+       for txt in line:
+              vcmd = root.register(jarCallback)
+              # text = tk.StringVar()
+              # text.set(txt)
+              # e =tk.Entry(master=frm_tmp,textvariable=text,validate="key",validatecommand=(vcmd,'%P'),width=20)
+              # e =tk.Entry(master=frm_tmp,validate="focusout",validatecommand=taskCallback,width=20)
+              e =tk.Entry(master=frm_tmp,validate="key",validatecommand=(vcmd, line[0], cnt, "%P"),width=20)
+              # e =tk.Entry(master=frm_tmp,width=20)
+              # e.insert(0,txt)
+              e.insert(0,txt)
+              e._id = line[0]
+              e.pack(side=tk.LEFT)
+              cnt = cnt + 1
+       frm_tmp.pack()
+editingReady = True
+###Packing
+greeting.pack()
+tables.pack()
+frm.pack()
+
+### Begin program
+print("Breakpoint")
+root.title('Moosh')
+root.geometry("1000x800")
+root.mainloop()
+
+
 
        
 
 def exit_handler():
+#     get_all_text_of_tasks(tables)
     print ('I am dying')
+    
     # for line in dailies:
     #     print(line)
+
+
 
 atexit.register(exit_handler)
 
 
        ### Packing
-       
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
